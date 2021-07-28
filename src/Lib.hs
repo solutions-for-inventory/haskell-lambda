182528184ad93751b431b1223f57d922059863ab
Jorge Anez <jorge.anez@coxautoinc.com>
2021-07-27 23:31:58 -0400

    init

---
+++ src/Lib.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE DeriveGeneric  #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
+module Lib where
+
+import GHC.Generics
+import Data.Aeson
+import Aws.Lambda
+
+data Person = Person
+  { personName :: String
+  , personAge :: Int
+  } deriving (Generic, FromJSON, ToJSON)
+
+--instance FromJSON Person
+--instance ToJSON Person
+
+handler :: Person -> Context () -> IO (Either String Person)
+handler person context =
+  if personAge person > 0 then
+    return (Right person)
+  else
+    return (Left "A person's age must be positive")
