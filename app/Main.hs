522cd97aebc5323f62058b17b472749519d30925
Jorge Anez <jorge.aniez@outlook.com>
2023-01-25 00:02:08 -0400

    grapql test

--- app/Main.hs
+++ app/Main.hs
@@ -12,4 +12,4 @@ main = do
     defaultDispatcherOptions
     (pure ())
     id
-    (addStandaloneLambdaHandler "handler" Lib.gqlSchemaHandler)
+    (addStandaloneLambdaHandler "handler" Lib.gqlHandler)
